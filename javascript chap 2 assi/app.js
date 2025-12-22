ad776e30563dbcafb2784565f4880dff415e58d5
NarmeenSheeraz <sheeraznarmeen@gmail.com>
2025-12-22 14:45:22 +0500

    modified chap1 and chap2

---
+++ javascript chap 2 assi/app.js
@@ -0,0 +1,24 @@
+var username;
+
+var myName = "Syeda NarmeenSheeraz";
+
+alert(myName);
+
+var myname = "Narmeen Sheeraz";
+var myage = "21";
+var mycourse = "Web Development"
+
+alert(myname);
+alert(myage);
+alert(mycourse);
+
+var pizza = "pizza \npizz \npiz \npi \np"
+alert(pizza);
+
+var email = "narmeen@icloud.com"
+alert(email);
+
+var book = "A smarter way to learn Javascript";
+alert("I am trying to learn from the Book " + book)
+
+document.write("Yah! I can write HTML content through Javascript")
