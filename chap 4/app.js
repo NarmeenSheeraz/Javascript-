55116c679a88c64cb8aacfcb4cd67079736993f7
NarmeenSheeraz <sheeraznarmeen@gmail.com>
2025-12-29 12:29:47 +0500

    chap9-11

--- chap 4/app.js
+++ chap 4/app.js
@@ -1,3 +1,19 @@
 var a, b, c;
 
 var myName;
+var userDetail;
+var myAge;
+var number;
+var myVar;
+
+// var 3name;
+// var %percentage;
+// var 0zero;
+// var {myNAME};
+// var Name4;
+
+document.write("<h1>" + "Rules for naming JS variables" + "</h1>" + "<br>" + 
+    "Variable names can only "
+);
+
+
