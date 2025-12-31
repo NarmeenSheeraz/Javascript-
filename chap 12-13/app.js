33555e6877b3bc8f48a34a37383288046a77c070
NarmeenSheeraz <sheeraznarmeen@gmail.com>
2025-12-31 12:53:48 +0500

    chap 12-13

---
+++ chap 12-13/app.js
@@ -0,0 +1,83 @@
+// question1
+// var ch = prompt("Enter a alphabet:");
+// var code = ch.charCodeAt(0);
+
+// if (code >= 48 && code <= 57) {
+//   console.log("It is a number");
+// } else if (code >= 65 && code <= 90) {
+//   console.log("It is an uppercase letter");
+// } else if (code >= 97 && code <= 122) {
+//   console.log("It is a lowercase letter");
+// } else {
+//   console.log("Invalid input");
+// }
+
+// question2
+// var num1 = parseInt(prompt("Enter first number:"));
+// var num2 = parseInt(prompt("Enter second number:"));
+
+// if (num1 > num2) {
+//   console.log(num1 + " is larger");
+// } else if (num2 > num1) {
+//   console.log(num2 + " is larger");
+// } else {
+//   console.log("Both numbers are equal");
+// }
+
+
+// question3
+// var number = parseInt(prompt("Enter a number:"));
+
+// if (number > 0) {
+//   console.log("Positive number");
+// } else if (number < 0) {
+//   console.log("Negative number");
+// } else {
+//   console.log("Zero");
+// }
+
+
+// question4
+// var char = prompt("Enter a character:").toLowerCase();
+
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// question5
+// var password = prompt("Enter ur password");
+// var correctpas = "Color12";
+// if (password === correctpas){
+//          alert("Correct! the password you entered matches the original password");
+// } else if (password == "" ){
+//   alert("Please enter ur password")
+// }else if(password != correctpas) {
+//   alert("Incorrect password")
+// }
+
+// question6
+// var greeting;
+// var hour = 13;
+// if(hour < 18 ){
+//   greeting = "Good Day!";
+// } else {
+//        greeting = "Good Evening!";
+// }
+
+
+// question7
+// var time = prompt("enter time format like 1900=7pm");
+// if(time >= 0000 && time < 1200){
+//   alert("Good Morning!");
+// }
+// if(time >= 1200 && time < 1700){
+//   alert("Good afternoon!");
+// }
+// if(time >= 1700 && time < 2100){
+//   alert("Good evening!");
+// }
+// if(time >= 2100 && time <= 2359){
+//   alert("Good night!");
+// }
--- chap 9-11/app.js
+++ chap 9-11/app.js
@@ -1,14 +1,14 @@
 var cityQues = prompt("Enter your city name.");
-if( cityQues == "karachi"){
-    alert("Welcome to city of lights.");
+if (cityQues == "karachi") {
+  alert("Welcome to city of lights.");
 }
 
 var gender = prompt("What’s your gender?");
-if( gender == "male"){
-    alert("Good morning Sir!");
+if (gender == "male") {
+  alert("Good morning Sir!");
 }
- else if( gender == "female"){
-    alert("Good morning Mam!");
+else if (gender == "female") {
+  alert("Good morning Mam!");
 }
 
 let color = prompt("Enter traffic signal color").toLowerCase();
@@ -62,26 +62,61 @@ if (number % 2 === 0) {
   alert("The number is odd");
 }
 
+var sub1 = +prompt("enter ur subject1 marks");
+var sub2 = +prompt("enter ur subject2 marks");
+var sub3 = +prompt("enter ur subject3 marks");
+var totalMarks = 300;
+var obtainedMarks = sub1 + sub2 + sub3;
+var percentage = (obtainedMarks / totalMarks) * 100;
+var grade, remarks;
+if (percentage > 100) {
+  percentage = "Invalid";
+  grade = "Invalid";
+  remarks = "enter ur percentage in 100s";
+}
+else if (percentage >= 80) {
+  grade = "A-one";
+  remarks = "Excellent";
+}
+else if (percentage >= 70) {
+  grade = "A";
+  remarks = "Good";
+}
+else if (percentage >= 60) {
+  grade = "B";
+  remarks = "you need improvement!";
+}
+else if (percentage >= 50) {
+  grade = "Fail";
+  remarks = "Sorry";
+}
+
+document.write("<h1>Grade Mark Sheet</h1>");
+document.write("Total Marks" + totalMarks + "<br>");
+document.write("Obtained Marks" + obtainedMarks + "<br>");
+document.write("Percantage" + percentage + "<br>");
+document.write("Grade" + grade + "<br>");
+document.write("Remarks" + remarks + "<br>");
 
 
 
 // Create a simple calculator using prompt() that takes two numbers and an operator (+, -, , /), and performs the correct operation
 function myFunction10() {
-    var vb1 = Number(prompt("Enter first number"));
-    var vb2 = Number(prompt("Enter second number"));
-    var operater = prompt("Enter operater +,-,/,*");
-    var result;
+  var vb1 = Number(prompt("Enter first number"));
+  var vb2 = Number(prompt("Enter second number"));
+  var operater = prompt("Enter operater +,-,/,*");
+  var result;
 
-    if (operater === "+") {
-        result = vb1 + vb2;
-    } else if (operater === "*") {
-        result = vb1 * vb2;
-    } else if (operater === "-") {
-        result = vb1 - vb2;
-    } else if (operater === "/") {
-        result = vb1 / vb2;
-    } else {
-        result = "Invalid operater";
-    }
-    alert(result);
+  if (operater === "+") {
+    result = vb1 + vb2;
+  } else if (operater === "*") {
+    result = vb1 * vb2;
+  } else if (operater === "-") {
+    result = vb1 - vb2;
+  } else if (operater === "/") {
+    result = vb1 / vb2;
+  } else {
+    result = "Invalid operater";
+  }
+  alert(result);
 }
