2114966d7bf65bb7c9f324d32284c6687709eef5
NarmeenSheeraz <sheeraznarmeen@gmail.com>
2026-01-12 13:17:51 +0500

    chap 17-20

---
+++ Loop/app.js
@@ -0,0 +1,365 @@
+// document.write(`<div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
+//             <div class="col">
+//                 <div class="card">
+//                     <img src="img/Container (3).png" class="card-img-top" alt="...">
+//                     <div class="card-body">
+//                         <div class="  d-flex ">
+//                             <p class="bold">Travel</p>
+//                             <p class="colorgray ms-2">16 March 2023</p>
+//                         </div>
+//                         <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+//                         <p class="card-text colorgray">Travelling in sea has many advantages. Some of the advantages of
+//                             transporting
+//                             goods by sea include: you can ship large volumes at costs </p>
+//                         <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+//                                 More...</small>
+//                         </p>
+//                     </div>
+
+//                 </div>
+//             </div>
+//             <div class="col">
+//                 <div class="card">
+//                     <img src="img/Container (4).png" class="card-img-top" alt="...">
+//                     <div class="card-body">
+//                         <div class="  d-flex ">
+//                             <p class="bold">Travel</p>
+//                             <p class="colorgray ms-2">16 March 2023</p>
+//                         </div>
+//                         <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+//                         <p class="card-text colorgray">Travelling in sea has many advantages. Some of the advantages
+//                             of
+//                             transporting
+//                             goods by sea include: you can ship large volumes at costs </p>
+//                         <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+//                                 More...</small></p>
+//                     </div>
+
+//                 </div>
+//             </div>
+//             <div class="col">
+//                 <div class="card">
+//                     <img src="img/container(5).png" class="card-img-top" alt="...">
+//                     <div class="card-body">
+//                         <div class="  d-flex ">
+//                             <p class="bold">Travel</p>
+//                             <p class="colorgray ms-2">16 March 2023</p>
+//                         </div>
+//                         <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+//                         <p class="card-text colorgray">Travelling in sea has many advantages. Some of the
+//                             advantages of
+//                             transporting
+//                             goods by sea include: you can ship large volumes at costs </p>
+//                         <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+//                                 More...</small></p>
+//                     </div>
+
+//                 </div>
+//             </div>
+
+
+//         </div>`)
+
+// for(var i = 1; i <= 100; i++){
+//      document.write(`<div class="container">
+//         <div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
+//     <div class="col">
+
+//         <div class="card"> 
+//             <img src="https://narmeensheeraz.github.io/Module-1-Hackathon/img/Image%20Placeholder.svg" class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <div class="  d-flex ">
+//                     <p class="bold">Travel</p>
+//                     <p class="colorgray ms-2">16 March 2023</p>
+//                 </div>
+//                 <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+//                 <p class="card-text colorgray">Travelling in sea has many advantages. Some of the advantages of
+//                     transporting
+//                     goods by sea include: you can ship large volumes at costs </p>
+//                 <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+//                         More...</small>
+//                 </p>
+//             </div>
+
+//         </div>
+//     </div>
+//     <div class="col">
+//         <div class="card">
+//             <img src="https://narmeensheeraz.github.io/Module-1-Hackathon/img/Image%20Placeholder%20(1).svg" class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <div class="  d-flex ">
+//                     <p class="bold">Travel</p>
+//                     <p class="colorgray ms-2">16 March 2023</p>
+//                 </div>
+//                 <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+//                 <p class="card-text colorgray">Travelling in sea has many advantages. Some of the advantages
+//                     of
+//                     transporting
+//                     goods by sea include: you can ship large volumes at costs </p>
+//                 <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+//                         More...</small></p>
+//             </div>
+
+//         </div>
+//     </div>
+//     <div class="col">
+//         <div class="card">
+//             <img src="https://narmeensheeraz.github.io/Module-1-Hackathon/img/Image%20Placeholder%20(2).svg" class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <div class="  d-flex ">
+//                     <p class="bold">Travel</p>
+//                     <p class="colorgray ms-2">16 March 2023</p>
+//                 </div>
+//                 <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+//                 <p class="card-text colorgray">Travelling in sea has many advantages. Some of the
+//                     advantages of
+//                     transporting
+//                     goods by sea include: you can ship large volumes at costs </p>
+//                 <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+//                         More...</small></p>
+//             </div>
+
+//         </div>
+//     </div>
+
+
+// </div>
+// </div>`)
+
+// }
+
+var num = 22;    
+
+
+// for(var i = 1; i<=10; i++){
+//     document.write(`<h1>${num} * ${i} = ${num * i}</h1>`) agr console ya document {} kai adr hi karwa daien gai tu 10times plus hoke alag alag aaye gi 
+//      num = num + i;
+//      document.write(num);
+// }
+
+
+// for(var i = 1; i<=10; i++){
+//     document.write(`<h1>${num} * ${i} = ${num * i}</h1>`) agr console ya document {} kai bahar karwien gai tu 10times plus hoke aaye gi aik hi bar 
+//      num = num + i;
+// }
+//      document.write(num);
+
+
+
+
+
+
+//         / document.write(`<div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
+// //             <div class="col">
+// //                 <div class="card">
+// //                     <img src="img/Container (3).png" class="card-img-top" alt="...">
+// //                     <div class="card-body">
+// //                         <div class="  d-flex ">
+// //                             <p class="bold">Travel</p>
+// //                             <p class="colorgray ms-2">16 March 2023</p>
+// //                         </div>
+// //                         <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+// //                         <p class="card-text colorgray">Travelling in sea has many advantages. Some of the advantages of
+// //                             transporting
+// //                             goods by sea include: you can ship large volumes at costs </p>
+// //                         <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+// //                                 More...</small>
+// //                         </p>
+// //                     </div>
+
+// //                 </div>
+// //             </div>
+// //             <div class="col">
+// //                 <div class="card">
+// //                     <img src="img/Container (4).png" class="card-img-top" alt="...">
+// //                     <div class="card-body">
+// //                         <div class="  d-flex ">
+// //                             <p class="bold">Travel</p>
+// //                             <p class="colorgray ms-2">16 March 2023</p>
+// //                         </div>
+// //                         <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+// //                         <p class="card-text colorgray">Travelling in sea has many advantages. Some of the advantages
+// //                             of
+// //                             transporting
+// //                             goods by sea include: you can ship large volumes at costs </p>
+// //                         <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+// //                                 More...</small></p>
+// //                     </div>
+
+// //                 </div>
+// //             </div>
+// //             <div class="col">
+// //                 <div class="card">
+// //                     <img src="img/container(5).png" class="card-img-top" alt="...">
+// //                     <div class="card-body">
+// //                         <div class="  d-flex ">
+// //                             <p class="bold">Travel</p>
+// //                             <p class="colorgray ms-2">16 March 2023</p>
+// //                         </div>
+// //                         <h5 class="card-title bold">8 Rules of Travelling In Sea You Need To Know</h5>
+// //                         <p class="card-text colorgray">Travelling in sea has many advantages. Some of the
+// //                             advantages of
+// //                             transporting
+// //                             goods by sea include: you can ship large volumes at costs </p>
+// //                         <p class="card-text"><small class="text-body-secondary colorpurple pointer readmore bold">Read
+// //                                 More...</small></p>
+// //                     </div>
+
+// //                 </div>
+// //             </div>
+
+
+// //         </div>`)
+
+
+
+// var iceImg1 = ["https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659245260.png", 
+//     "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659258143.png", 
+//     "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659272276.png",
+// ];
+// var iceHea1 = ["Vanilla Swirl", "Vanilla Brownie Swirl", "Vanilla Cookies Swirl"];
+// var icePrice1 = [170, 240, 240]
+
+
+// for (var i = 0; i < iceImg1.length; i++) {
+//     document.write(`  
+//         <div class="container">
+
+//         <div class="card " style="width: 18rem;">
+//             <img src="${iceImg1[i]}" class="card-img-top img-fluid" alt="...">
+//             <div class="card-body">
+//                 <h4 class="text-center fw-bold">${iceHea1[i]}</h4>
+//             </div>
+//             <div class="line my-4 mx-auto"></div>
+//             <div class="pricing my-3">
+//                 <h5 class="text-center fw-bold">Rs . ${icePrice1[i]}.00</h5>
+//                 <button class="btn text-white fw-bold bg-dark rounded-pill mx-auto d-block">Add to cart</button>
+//             </div>
+//         </div>
+//     </div> 
+//     `)
+
+// }
+
+var iceImg1 = [
+    "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659245260.png",
+    "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659258143.png",
+    "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659272276.png",
+    "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659245260.png",
+    "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659258143.png",
+    "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659272276.png",
+    "https://g-cdn.blinkco.io/ordering-system/55860/dish_image/1738659308485.png"
+];
+
+var iceHea1 = [
+    "Vanilla Swirl",
+    "Vanilla Brownie Swirl",
+    "Vanilla Cookies Swirl",
+    "Vanilla Swirl",
+    "Vanilla Brownie Swirl",
+    "Vanilla Cookies Swirl",
+    "Choclate"
+];
+
+var icePrice1 = [170, 240, 240, 170, 240, 240, 90];
+
+/* Parent Flex Container */
+// document.write(`
+//   <div class="container my-5">
+//     <div class="row d-flex justify-content-center gap-4">
+// `);
+
+/* Cards Loop */
+// for (var i = 0; i < iceImg1.length; i++) {
+//     document.write(`
+//     <div class="card shadow " style="width: 18rem;">
+//       <img src="${iceImg1[i]}" class="card-img-top w-75 d-block mx-auto rounded bg-light mt-2" alt="ice cream">
+
+//       <div class="card-body">
+//         <h4 class="text-center fw-bold">${iceHea1[i]}</h4>
+//       </div>
+
+//       <div class="line mb-3 mx-auto"></div>
+
+//       <div class="pricing text-center mb-3">
+//         <h5 class="fw-bold">Rs. ${icePrice1[i]}.00</h5>
+//         <button class="btn btn-dark rounded-pill fw-bold px-4">
+//           Add to cart
+//         </button>
+//       </div>
+//     </div>
+//   `);
+// }
+
+/* Close Parent */
+// document.write(`
+//     </div>
+//   </div>
+// `);
+
+// document.write(`<div class="container my-5">
+//   <div class="row d-flex justify-content-center gap-4">
+//   `)
+
+//  for(var i = 0; i < iceImg1.length; i++){
+//   document.write(`<div class="card shadow " style="width: 18rem;">
+//        <img src="${iceImg1[i]}" class="card-img-top w-75 d-block mx-auto rounded bg-light mt-2" alt="ice cream">
+
+//       <div class="card-body">
+//                <h4 class="text-center fw-bold">${iceHea1[i]}</h4>
+//        </div>
+
+//        <div class="line mb-3 mx-auto"></div>
+
+//        <div class="pricing text-center mb-3">
+//          <h5 class="fw-bold">Rs. ${icePrice1[i]}.00</h5>
+//          <button class="btn btn-dark rounded-pill fw-bold px-4">
+//            Add to cart
+//          </button>
+//        </div>
+//      </div>
+//     `)
+//  }
+
+//  document.write(`</div></div>`)
+
+
+
+
+//  var num = 9 ;
+//  for(var j = 1; j <= 10; j++){
+//   document.write(num + " " + "*" + " " + j + " " + "=" + " " + (num * j) + "<br>")
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ chap 17-20/app.js
@@ -0,0 +1,25 @@
+// var arrDim = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];   question1
+
+// for(var i = 1; i <= 10; i++){
+//     document.write(i + "<br>")
+// } qiestion 2
+
+// qiestion 3
+// var tabNum = +prompt("Enter a number for table!") || 5; 
+// var tabLen = +prompt("Enter a number for table length!") || 10;
+// for( var i= 1; i <= tabLen; i++ ){
+//     document.write( tabNum + "x" + i + "=" + tabNum * i + "<br>")
+// }
+
+// question 4
+// var fruits = ["apple", "mango", "banana", "grapes", "cherry"]
+// for(var i = 0; i < fruits.length; i++){
+//     document.write("Element at index " + i + " is " + fruits[i] + "<br>")
+// }
+
+// quetion 5
+var coun = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+for(var i = 0; i < coun.length; i++){
+    document.write(i + ",")
+
+}
